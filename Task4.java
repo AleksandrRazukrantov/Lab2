7eaf46d23769d776e00be149df3836088cf6594d
AleksandrRazukrantov <a1razukrantov@mail.ru>
2025-11-07 00:20:19 +0300

    Update Task4.java

--- Task4.java
+++ Task4.java
@@ -5,24 +5,24 @@ public class Main {
     public static void main(String[] args) {
 
        int n = in.nextInt();
-       int a;
-       boolean f = false;
-       int[] b = new int[101];
+        int a;
+        boolean f = true;
+        int[] b = new int[101];
 
-       for(int i = 0; i < n; i++){
-           a = in.nextInt();
-           b[a]++;
-       }
-       for(int i = 10; i < 101; i++){
-           if (b[i] == i){
-               System.out.print(b[i] + " ");
-               f = true;
-           }
-       }
+        for(int i = 0; i < n; i++){
+            a = in.nextInt();
+            b[a]++;
+        }
+        for(int i = 10; i < 101; i++){
+            if (b[i] == i){
+                System.out.print(b[i] + " ");
+                f = false;
+            }
+        }
 
-       if (f == false){
-           System.out.println("NO");
-       }
+        if (f){
+            System.out.println("NO");
+        }
 
     }
 }
